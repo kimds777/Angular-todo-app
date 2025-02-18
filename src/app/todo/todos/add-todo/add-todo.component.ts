2219ae04abff8d9293c0698cee2fc7b7d891208a
Daseul Kim <tina27849@gmail.com>
2025-02-18 17:09:56 +0900

    Feat : Component, Global css 적용

--- src/app/todo/todos/add-todo/add-todo.component.ts
+++ src/app/todo/todos/add-todo/add-todo.component.ts
@@ -8,10 +8,37 @@ import {FormsModule} from '@angular/forms';
     FormsModule
   ],
   template: `
+    <button (click)="addTodo(newText)">+</button>
     <input type="text" placeholder="할 일 추가" [(ngModel)]="newText">
-    <button (click)="addTodo(newText)">ADD</button>
   `,
-  styles: ``
+  styles: [`
+    :host {
+      display: block;
+      padding: 16px 16px 16px 10px;
+      background-color: white;
+    }
+
+    input {
+      display: inline-block;
+      font-size: 18px;
+      border: none;
+    }
+
+    input:focus, button:focus {
+      outline: none;
+    }
+
+    button {
+      width: 24px;
+      height: 24px;
+      border-radius: 24px;
+      color: white;
+      font-size: 16px;
+      line-height: 17px;
+      border: 1px solid dimgray;
+      background-color: darkblue;
+    }
+  `]
 })
 export class AddTodoComponent {
   @Output() onTodoAdded = new EventEmitter();
--- src/app/todo/todos/todo/todo.component.ts
+++ src/app/todo/todos/todo/todo.component.ts
@@ -6,9 +6,53 @@ import { Todo } from '../../share/todo.model';
   standalone: true,
   imports: [],
   template: `
-    <input type="checkbox" [checked]="todo.done"> {{ todo.text }}
+    <input type="checkbox" [checked]="todo.done"> <label>{{ todo.text }}</label>
   `,
-  styles: []
+  styles: [`
+    :host {
+      display: block;
+      padding: 16px;
+      color: darkgray;
+      background-color: white;
+    }
+
+    input {
+      position: relative;
+    }
+
+    input:before {
+      content: "";
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background-color: white;
+      border-radius: 20px;
+      position: absolute;
+      top: -6px;
+      left: -8px;
+      border: 1px solid dimgray;
+    }
+
+    input:checked:after {
+      content: '\\2713';
+      display: inline-block;
+      font-size: 18px;
+      width: 20px;
+      height: 20px;
+      background-color: dimgray;
+      border-radius: 20px;
+      position: absolute;
+      top: -6px;
+      left: -8px;
+      border: 1px solid dimgray;
+      text-align: center;
+      color: white;
+    }
+
+    input:checked + label {
+      text-decoration: line-through;
+    }
+  `]
 })
 export class TodoComponent {
   @Input() todo!: Todo;
